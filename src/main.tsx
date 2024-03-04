32203aedbd4ef44b094ab8a3848fab047b121b69
juratali <juratbekxudayberganov9@gmail.com>
2024-03-04 15:16:00 +0500

    twa sdk integration

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,19 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import { TelegramProvider } from "./providers/Telegram.tsx";
+
+import WebApp from "@twa-dev/sdk";
 
 export const queryClient = new QueryClient();
+
+WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <Toaster />
-        <TelegramProvider>
-          <App />
-        </TelegramProvider>
+        <App />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,12 @@ import Input from "../components/ui-custom/input";
 import { FetchMatchesType } from "@/types";
 import { useEffect, useState } from "react";
 import { AiOutlineLoading3Quarters as Loader } from "react-icons/ai";
-import { useTelegram } from "@/providers";
+import WebApp from "@twa-dev/sdk";
 
 const Home = () => {
   const [keyword, setKeyword] = useState("");
-  const { user } = useTelegram();
+
+  console.log(WebApp.initDataUnsafe);
 
   const fetchMatchQuery = useFetchMatches(
     {
@@ -18,34 +19,30 @@ const Home = () => {
       limit: 10,
       keyword,
     },
-    user?.id!
+    WebApp.initDataUnsafe.user?.id!
   );
 
   useEffect(() => {
-    if (user) {
-      fetchMatchQuery.refetch();
-    }
-  }, [keyword, user]);
+    fetchMatchQuery.refetch();
+  }, [keyword]);
 
   const matchesList: FetchMatchesType = fetchMatchQuery.data?.data;
 
-  return user ? (
-    fetchMatchQuery.isLoading ? (
-      <div className="flex flex-col items-center mt-20">
-        <Loader className="animate-spin max-xl:text-2xl text-3xl text-purple-700 text-center" />
+  return fetchMatchQuery.isLoading ? (
+    <div className="flex flex-col items-center mt-20">
+      <Loader className="animate-spin max-xl:text-2xl text-3xl text-purple-700 text-center" />
+    </div>
+  ) : (
+    <div className="mt-5 max-xl:mt-3 grid max-xl:grid-cols-1 grid-cols-3 gap-5 max-xl:gap-3">
+      <div className="xl:hidden">
+        <Input onChange={setKeyword} />
       </div>
-    ) : (
-      <div className="mt-5 max-xl:mt-3 grid max-xl:grid-cols-1 grid-cols-3 gap-5 max-xl:gap-3">
-        <div className="xl:hidden">
-          <Input onChange={setKeyword} />
-        </div>
 
-        {matchesList?.matches?.map((match) => (
-          <MatchCard action="IN_LIST" key={match.id} data={match} />
-        ))}
-      </div>
-    )
-  ) : null;
+      {matchesList?.matches?.map((match) => (
+        <MatchCard action="IN_LIST" key={match.id} data={match} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
