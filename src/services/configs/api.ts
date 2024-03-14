09958f295c85464c12cbdc6c48898c29571ac37a
juratali <juratbekxudayberganov9@gmail.com>
2024-03-14 10:27:25 +0500

    v2 deployment

--- src/services/configs/api.ts
+++ src/services/configs/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const baseURL_LOCAL = "http://localhost:3002/api/v1";
-// const baseURL_PROD = "https://api.ifut.uz/api/v1";
+// const baseURL_LOCAL = "http://localhost:3002/api/v1";
+const baseURL_PROD = "https://api.ifut.uz/api/v1";
 
 const api = axios.create({
-  baseURL: baseURL_LOCAL,
+  baseURL: baseURL_PROD,
 });
 
 api.interceptors.request.use(async (config) => {
